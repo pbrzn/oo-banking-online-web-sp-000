52a0270127001b1363b994700fc74314a91175b9
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-03 17:38:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,7 @@ class Transfer
       sender.balance-=@amount
       receiver.balance+=@amount
       self.status = "complete"
+    elsif sender.balance
     else
       self.status="rejected"
       "Transaction rejected. Please check your account balance."
