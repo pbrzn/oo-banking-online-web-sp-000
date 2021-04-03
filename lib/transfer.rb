42be6171dc7d10c0a7c26fe35b881a9bf8f2eddc
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-03 17:53:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,5 +23,11 @@ class Transfer
     end
   end
   
-  
+  def reverse_transfer
+    if self.status=="complete"
+      sender.balance+=amount
+      receiver.balance-=amount
+      self.status="reversed"
+    end
+  end
 end
